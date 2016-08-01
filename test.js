79843ab6761eaab9de28ca61655938cdc82c4c7e
Katherine Erickson <katherine.erickson@gmail.com>
2016-08-01 15:35:31 -0400

    More crap

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
-var underlay = $("#underlay");
-var input = $("#masked-input");
-console.log(input);
-
-input.on("click", function() {
-  console.log("keying");
+$(document).ready(function() {
+  doStuff();
 });
+
+function doStuff() {
+  var underlay = $("#underlay");
+  var input = $("#masked-input");
+
+  // This works
+  underlay.css("background", "green");
+
+  // This does not
+  underlay.on("click", function() {
+    console.log("clicking");
+  });
+
+  /*
+  input.on("click", function() {
+    console.log("keying");
+  });
+  */
+}
