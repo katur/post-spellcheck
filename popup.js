03675a7d784a1f2e73a6a8e045de7b503ac755e1
Katherine Erickson <katherine.erickson@gmail.com>
2016-08-03 15:06:30 -0400

    Cleanup

--- popup.js
+++ popup.js
@@ -2,25 +2,18 @@ TEXT_INPUT_SELECTOR = "input[type=text], textarea, [contenteditable]"
 
 
 $(document).ready(function() {
+  activateButton();
+});
+
+
+function activateButton() {
   var button = $("#disable-spellcheck-button");
 
   button.on("click", function() {
-    /*
-    // Here is how you can get the current tab.
-    // Avoid chrome.tabs.getCurrent and chrome.tabs.getSelected because
-    // they are deprecated or soon-to-be.
+    // Color this popup
+    $("body").css("background", "orange");
 
-    const CURRENT_TAB_QUERY_PARAMS = {
-      currentWindow: true,
-      active: true,
-    }
-
-    chrome.tabs.query(CURRENT_TAB_QUERY_PARAMS, function (tabs) {
-      tab = tabs[0]; // Query returns a list, so extract the lone result
-    });
-    */
-
-    // Defaults to active tab of current window, so no need for above query
+    // Defaults to active tab of current window
     chrome.tabs.executeScript({
       file: "jquery.js",
       allFrames: true,
@@ -29,9 +22,6 @@ $(document).ready(function() {
         file: "test-script.js",
         allFrames: true,
       });
-    }),
-
-    // affects this popup
-    $("body").css("background", "orange");
+    });
   });
-});
+}
